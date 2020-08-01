8a4dc89581a0fa53748ce252cb8f39c74cf0bee3
piyushsrii <piyushnamo888@gmail.com>
2020-08-01 23:50:19 +0530

    commit in uc3

--- 19teenth.sh
+++ 19teenth.sh
@@ -42,6 +42,44 @@ public class Employee2
 }
 }
 
+###UserCase-3###
+Problem Statement------>Add Part time Employee & Wage-Assume Part time Hour is 8
+public class EmployeeSwitch4
+{
+        public static final int IS_PART_TIME=1;
+        public static final int IS_FULL_TIME=2;
+        public static final int EMP_RATE_PER_HOUR=20;
+        public static final int NUM_OF_WORKING_DAYS=5;
+        public static void main(String args[])
+        {
+        int empHRS=0;
+        int empWage=0;
+        int totalEmpWage=0;
+        int totalEmpHrs=0;
+        int totalworkingdays=0;
+        while(totalworkingdays<NUM_OF_WORKING_DAYS)
+        {
+         totalworkingdays++;
+         int empcheck=(int)Math.floor(Math.random()*10)%3;
+         switch(empcheck)
+        {
+           case IS_FULL_TIME:
+           empHRS=8;
+           break;
+           case IS_PART_TIME:
+           empHRS=4;
+           break;
+           default:
+           empHRS=0;
+        }
+         totalEmpHrs+=empHRS;
+         System.out.println("totalworkingdays"+totalworkingdays+" "+"totalEmpHrs"+totalEmpHrs);
+        }
+         totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+         System.out.println("total employee wage"+totalEmpWage);
+        }
+}
+
 
 
 
