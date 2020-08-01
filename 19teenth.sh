abb8703185275604d7c8d5081b17d5ff8b7471ed
piyushsrii <piyushnamo888@gmail.com>
2020-08-01 20:18:53 +0530

    commit in uc1

--- 19teenth.sh
+++ 19teenth.sh
@@ -1,1 +1,25 @@
 Welcome into 19th Assignment
+Problem statement----->(Check Employee is Present or Absent- Use ((RANDOM)) for Attendance Check)
+public class Employee1
+{
+    public static void main(String[] args)
+    {
+          double empcheck=Math.floor(Math.random()*10)%2;
+          if(empcheck==1)
+          {
+             System.out.println("Employee is present");
+          }
+          else
+          {
+             System.out.println("Employee is not present");
+          }
+    }
+}
+
+
+
+
+
+
+
+
