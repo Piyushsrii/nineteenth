d0b3797d482b75ca4e6abae4b1ed2beb0f7d4e07
piyushsrii <piyushnamo888@gmail.com>
2020-08-02 00:11:56 +0530

    commit in uc6

--- 19teenth.sh
+++ 19teenth.sh
@@ -112,7 +112,8 @@ public class EmployeeSwitch4
         }
 }
 
-
+###UserCase-6###
+Problem Statement---->(Calculate Wages till a condition of total working hours or days is reached for a month - Assume 100 hours and 20 days)
 
 
 
